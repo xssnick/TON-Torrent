cb6533fff051de86d518e59a1bf9a9727d429bd0
Oleg Baranov <xssnick8@gmail.com>
2023-04-09 12:58:14 +0400

    Windows hook once windows

--- oshook/hook_other.go
+++ oshook/hook_other.go
@@ -3,30 +3,34 @@
 package oshook
 
 import (
-	"net/url"
 	"os"
 	"strings"
-	"time"
+	"sync"
 )
 
+var once sync.Once
+
 func HookStartup(cbFile func([]byte), cbHash func(string), dbg func(s string)) {
-	go func() {
-		time.Sleep(1 * time.Second)
-		dbg(strings.Join(os.Args, " "))
-	}()
-	if len(os.Args) > 1 {
-		go func() { // to not block main thread
-			if strings.HasPrefix(os.Args[1], "tonbag://") || strings.HasPrefix(os.Args[1], "tonstorage://") {
-				u, err := url.Parse(os.Args[1])
-				if err == nil {
-					cbHash(u.Host)
+	once.Do(func() {
+		if len(os.Args) > 1 {
+			go func() {
+				time.Sleep(1 * time.Second)
+				dbg(strings.Join(os.Args, " "))
+			}()
+
+			go func() { // to not block main thread
+				if strings.HasPrefix(os.Args[1], "tonbag://") || strings.HasPrefix(os.Args[1], "tonstorage://") {
+					u, err := url.Parse(os.Args[1])
+					if err == nil {
+						cbHash(u.Host)
+					}
+				} else {
+					data, err := os.ReadFile(os.Args[1])
+					if err == nil {
+						cbFile(data)
+					}
 				}
-			} else {
-				data, err := os.ReadFile(os.Args[1])
-				if err == nil {
-					cbFile(data)
-				}
-			}
-		}()
-	}
+			}()
+		}
+	})
 }
